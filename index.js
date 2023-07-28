a89ebf408f987a9dfb1310b9fa2a70742e303787
Christine Lea <christine87@live.com.au>
2023-07-28 16:56:55 +1000

    add some TODOs

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs/promises");
 const shapes = require("./lib/shapes.js");
+
+// TODO create a questions array
+// TODO inquirer.prompt() to ask questions
+// TODO use answers in matching Shape class
+// TODO writeFile
