f384953bbc14331ebd816d27cc09a70b516ce636
Christine Lea <christine87@live.com.au>
2023-07-29 15:28:19 +1000

    add shape class and render svg

--- index.js
+++ index.js
@@ -6,52 +6,53 @@ const shapes = require("./lib/shapes.js");
 // TODO create a questions array
 const questions = [
 	{
-		name: "text",
-		type: "input",
-		message: "TEXT: Enter up to three characters to add to your logo",
-		validate(answer) {
-			// check no more than 3 characters
-		},
-	},
-	{
-		name: "textColor",
-		type: "input",
-		message:
-			"TEXT COLOR: What color do you want the text to be?  Enter a color keyword OR a hex color ",
-		// validate
-	},
-	{
 		name: "shape",
 		type: "list",
 		message: "What shape do you want to use for your logo?",
 		choices: ["Circle", "Triangle", "Square"],
-		// validate
 	},
 	{
 		name: "shapeFill",
 		type: "input",
 		message:
 			"SHAPE COLOR: What color do you want the shape to be?  Enter a color keyword OR a hex color ",
-		// validate
+	},
+	{
+		name: "text",
+		type: "input",
+		message: "TEXT: Enter up to three characters to add to your logo",
+		validate(answer) {
+			// check no more than 3 characters
+			if (answer.length >= 4) {
+				console.log(
+					"Logo text must be no longer than three characters."
+				);
+			} else {
+				return true;
+			}
+		},
+	},
+	{
+		name: "textColor",
+		type: "input",
+		message:
+			"TEXT COLOR: What color do you want the text to be? Enter a color keyword OR a hex color ",
 	},
 ];
-// if (data.shape === "circle") {data.shape = circle}
+// function writeFile() {
+// 	fs.writeFile("./examples/logo.svg", )
+// }
+async function init() {
+	const data = await inquirer.prompt(questions);
 
-const shapes = {
-	circle: `circle cx="150" cy="100" r="70"`,
-	square: `rect width="130" height="130" x="80" y="30"`,
-    triangle: `polygon points="150, 1 274, 182 26, 182"`,
-};
-const color = `<text
-		x="150"
-		y="125"
-		font-size="60"
-		text-anchor="middle"
-		fill="${data.textColor}">`;
-const { circle, square, triangle } = shapes;
-// const circle = 
+	const shape = await shapes(data);
+	console.log(shape);
+}
+init();
+
 // TODO inquirer.prompt() to ask questions
 // TODO use answers in matching Shape class
 // TODO writeFile
 // set shape
 // TODO hex color
+// write svg 
