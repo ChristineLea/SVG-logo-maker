a53394165f21274bc307ac3055aeb520d9a2018f
Christine Lea <christine87@live.com.au>
2023-07-29 18:20:47 +1000

    add color to validate responses

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ const questions = [
 		type: "input",
 		message: "Enter a Named Color or Hex Color for the logo shape :",
 		validate: function (answer) {
-			
-		if (validateHTMLColorName(answer) || validateHTMLColorHex(answer)) {
-			return true;
-		} else {
-			console.error("Enter a valid Color keyword or Hex Color.");
-		}
-		}
+			if (validateHTMLColorName(answer) || validateHTMLColorHex(answer)) {
+				return true;
+			} else {
+				console.error(
+					"\x1b[31m%s \x1b[0m",
+					"Enter a valid Color keyword or Hex Color."
+				);
+			}
+		},
 	},
 	{
 		name: "text",
@@ -34,6 +36,7 @@ const questions = [
 			// check no more than 3 characters
 			if (answer.length >= 4) {
 				console.log(
+					"\x1b[31m%s \x1b[0m",
 					"Logo text must be no longer than three characters."
 				);
 			} else {
@@ -45,32 +48,32 @@ const questions = [
 		name: "textColor",
 		type: "input",
 		message: "Enter a Named Color or Hex Color for the logo text :",
+		validate: function (answer) {
+			if (validateHTMLColorName(answer) || validateHTMLColorHex(answer)) {
+				return true;
+			} else {
+				console.error(
+					"\x1b[31m%s \x1b[0m",
+					"Enter a valid Color keyword or Hex Color."
+				);
+			}
+		},
 	},
 ];
 function writeFile(fileName, data) {
 	fs.writeFile(fileName, data, (err) => {
-		err
-			? console.error("\x1b[31m%s \x1b[0m", "Error!")
-			: console.log(
-					"\x1b[32m%s \x1b[0m",
-					"Successfully generated SVG Logo!"
-			  );
+		if (err) {
+			console.log("\x1b[31m%s \x1b[0m", "Error!");
+		}
 	});
+	console.log("\x1b[32m%s \x1b[0m", "Generated logo.svg");
 }
-
 
 async function init() {
 	const data = await inquirer.prompt(questions);
-
+	console.log("\x1b[33m%s \x1b[0m", "Preparing your logo ...");
 	const svgLogo = await shapes.generateMarkdown(data);
 
 	writeFile("./examples/logo.svg", svgLogo);
 }
 init();
-
-// TODO inquirer.prompt() to ask questions
-// TODO use answers in matching Shape class
-// TODO writeFile
-// set shape
-// TODO hex color
-// write svg
